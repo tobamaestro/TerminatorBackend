05cc8acaaf9cabeff2f3774040d25455395302a5
Aleksandar Andjelkovic <tobamaestro@hotmail.com>
2017-09-11 21:00:45 +0200

    Initial commit

---
+++ src/main/java/com/toba/terminator/terminatorbackend/TerminatorBackendApplication.java
@@ -0,0 +1,12 @@
+package com.toba.terminator.terminatorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerminatorBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/toba/terminator/terminatorbackend/TerminatorBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.toba.terminator.terminatorbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TerminatorBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
